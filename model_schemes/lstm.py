5f5e18974b7ce14eccdf52450c47e889a6dc40e9
Oren Ben <orenben088@gmail.com>
2024-02-06 22:58:13 +0200

    added lstm

--- model_schemes/lstm.py
+++ model_schemes/lstm.py
@@ -1,109 +1,98 @@
 import os
 import random
 import numpy as np
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+import pandas as pd
 import tensorflow as tf
+from lstm_data_prep import LstmDataPrep
+from utils import data_spliter
+from orig_lstm import Vanilla_LSTM
 
-class Vanilla_LSTM:
+N_STEPS = 5
+EPOCHS = 25
+BATCH_SIZE = 32
+VAL_SPLIT = 0.2
+PARAMS = [N_STEPS, EPOCHS, BATCH_SIZE, VAL_SPLIT]
+Q = 0.99 # quantile for upper control limit (UCL) selection
+
+class LstmModel:
     """
-    LSTM-based neural network for anomaly detection using reconstruction error as an anomaly score.
-
-    Parameters
-    ----------
-    params : list
-        A list containing various parameters for configuring the LSTM model.
-
-    Attributes
-    ----------
-    model : Sequential
-        The trained LSTM model.
-
-    Examples
-    --------
-    >>> from Vanilla_LSTM import Vanilla_LSTM
-    >>> PARAMS = [N_STEPS, EPOCHS, BATCH_SIZE, VAL_SPLIT]
-    >>> lstm_model = Vanilla_LSTM(PARAMS)
-    >>> lstm_model.fit(train_data, train_labels)
-    >>> predictions = lstm_model.predict(test_data)
+    Example usage:
+    model = LstmModel()
+    model.train(df)
+    preds = model.predict(df)
+    
     """
     
-    def __init__(self, params):
-        self.params = params
-        
-    def _Random(self, seed_value):
-
-        os.environ['PYTHONHASHSEED'] = str(seed_value)
-
-        
-        random.seed(seed_value)
-
-        
-        np.random.seed(seed_value)
-
-        tf.random.set_seed(seed_value)
-        
-    def _build_model(self):
-        self._Random(0)
-        
-        model = Sequential()
-        model.add(LSTM(100, 
-                       activation='relu', 
-                       return_sequences=True, 
-                       input_shape=(self.params[0], self.n_features)))
-        model.add(LSTM(100, 
-                       activation='relu'))
-        model.add(Dense(self.n_features))
-        model.compile(optimizer='adam', 
-                      loss='mae', 
-                      metrics=["mse"])
-        return model
+    def __init__(self,params= None, num_models=5) -> None:
+        self.params = params if params else PARAMS
+        self.num_models = num_models
+        self.partitioned_dfs = None
+        self.x_train = None
+        self.y_train = None
+        self.x_test= None 
+        self.y_test = None
+        self._y_pred = None
+        self.y_pred = None
+        self.features_pipe = None
+        self.residuals_train = None
+        self.trained_model =None
+        self.ucl = None
     
-    def fit(self, X, y):
-        """
-        Train the LSTM model on the provided data.
+    def train(self,train_set):
+        self.features_pipe   =LstmDataPrep().run()
+        self.x_train, self.y_train = self.features_pipe.fit_transform(train_set)
+        model =  Vanilla_LSTM(self.params)
+        model.fit(self.x_train,self.y_train)
+        self.trained_model = model
+        self.residuals_train = pd.DataFrame(self.y_train - self.trained_model.predict(self.x_train)).abs().sum(axis=1)
+        self.ucl = self.residuals_train.quantile(Q) * 5
 
-        Parameters
-        ----------
-        X : numpy.ndarray
-            Input data for training the model.
-        y : numpy.ndarray
-            Target data for training the model.
-        """
-        self.n_features = X.shape[2]
-        self.model = self._build_model()
-
-        early_stopping = EarlyStopping(patience=10, 
-                                       verbose=0)
-
-        reduce_lr = ReduceLROnPlateau(factor=0.1, 
-                                      patience=5, 
-                                      min_lr=0.0001, 
-                                      verbose=0)
-
-        self.model.fit(X, y,
-                  validation_split=self.params[3],
-                  epochs=self.params[1],
-                  batch_size=self.params[2],
-                  verbose=0,
-                  shuffle=False,
-                  callbacks=[early_stopping, reduce_lr]
-                  )
     
-    def predict(self, data):
-        """
-        Generate predictions using the trained LSTM model.
-
-        Parameters
-        ----------
-        data : numpy.ndarray
-            Input data for generating predictions.
-
-        Returns
-        -------
-        numpy.ndarray
-            Predicted output data.
-        """
+    
+    def predict(self,df:pd.DataFrame):
+        self.x_test, self.y_test = self.features_pipe[1:].transform(df)
+        self._y_pred = self.trained_model.predict(self.x_test)
+        residuals = pd.DataFrame(self.y_test - self._y_pred).abs().sum(axis=1)
+        self.y_pred = pd.Series((residuals > self.ucl).astype(int).values).fillna(0)
+        return self.y_pred
+    
+class MultiModel:
         
-        return self.model.predict(data)
+    def __init__(self,params,num_models:int=5) -> None:
+        self.params = params if params else PARAMS
+        self.num_models = num_models
+        self.train_set = None
+        self.models = {}
+        self.partitioned_dfs = None
+        self.predictions_df = None
+        self.y_test = None
+        
+    def train(self,train_set):
+    
+        self.partitioned_dfs = data_spliter(df=train_set,num_splits=self.num_models)
+
+        for i, df in enumerate(self.partitioned_dfs):
+            self.models[f'lstm_{i}']= {}
+            self.models[f'lstm_{i}']['trained_model'] = Vanilla_LSTM(self.params)
+            self.models[f'lstm_{i}']['trained_model'].train(df)
+            
+    def predict(self,df:pd.DataFrame)->pd.DataFrame:
+    
+        features_pipe= LstmDataPrep().run()
+        _ , self.y_test = features_pipe[1:].transform(df)
+    
+        preds_dfs = []
+        for model, _ in self.models.items():
+            self.models[model]['predictions'] = pd.Series(self.models[model]['trained_model'].predict(df))
+            preds_df = pd.DataFrame({model:self.models[model]['predictions']})
+            preds_dfs.append(preds_df)
+        
+        self.predictions_df =  (
+            pd.concat(preds_dfs,axis=1)
+            .assign(
+                avg_predicion = lambda df_ : df_.mean(axis=1),
+                median_predicion = lambda df_ : df_.median(axis=1),
+                max_predicion = lambda df_ : df_.max(axis=1),
+                )
+            )
+        return self.predictions_df
--- model_schemes/utils.py
+++ model_schemes/utils.py
@@ -1,5 +1,7 @@
+from typing import List
 import os
 import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
 
 def get_file_paths(directory = ".", extension = ".csv"):
     """
@@ -51,3 +53,36 @@ def load_dfs_by_source(data_dir:str,data_source:str)-> pd.DataFrame:
         # .drop(columns=['changepoint'])
         .sort_index()
     )
+
+
+class DropColumnTransformer(BaseEstimator, TransformerMixin):
+    
+    def __init__(self, columns_to_drop:List):
+        self.columns_to_drop = columns_to_drop
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Check if the DataFrame has the specified columns to drop
+        if not set(self.columns_to_drop).issubset(X.columns):
+            missing_cols = list(set(self.columns_to_drop) - set(X.columns))
+            raise ValueError(f"Columns to drop not found: {missing_cols}")
+
+        # Drop the specified columns
+        X_transformed = X.drop(columns=self.columns_to_drop, axis=1)
+
+        return X_transformed
+    
+def data_spliter(df,num_splits):
+    num_rows_per_part = len(df) // num_splits
+
+    parts = []
+    for i in range(num_splits):
+        if i < num_splits-1:
+            part = df.iloc[i * num_rows_per_part: (i + 1) * num_rows_per_part]
+        else:
+            # For the last part, include the remaining rows
+            part = df.iloc[i * num_rows_per_part:]
+        parts.append(part)
+    return parts
